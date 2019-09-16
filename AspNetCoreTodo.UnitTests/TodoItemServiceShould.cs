5642c1f36ce048c6b846c4fc48d568095432c9cf
chibuikekenneth <chibuikekenneth003@gmail.com>
2019-09-16 11:34:07 +0100

    unit testing for TodoItemServices

---
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            //Set up a context (connection to the "DB") for writing
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem {Title = "Testing?"}, fakeUser);
+            }
+
+            //use a seperate context to read data back from the "DB"
+              using (var inMemoryContext = new ApplicationDbContext(options))
+            {
+                var itemsIndatabase = await inMemoryContext.Items.CountAsync();
+                Assert.Equal(1, itemsIndatabase);
+                
+                var item = await inMemoryContext.Items.FirstAsync();
+                Assert.Equal("fake-000", item.UserId);
+                Assert.Equal("Testing?", item.Title);
+                Assert.Equal(false, item.IsDone);
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                 Assert.True(difference < TimeSpan.FromSeconds(3));
+            }
+            
+        }
+    }
+}
